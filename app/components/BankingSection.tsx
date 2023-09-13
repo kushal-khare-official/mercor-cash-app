ce472df54dddcfe5e1353b18a8f2e90a79c599c6
kushal-khare-official <kushalkhare.official@gmail.com>
2023-09-13 18:44:44 +0530

    Add Banking section

---
+++ app/components/BankingSection.tsx
@@ -0,0 +1,19 @@
+export default function BankingSection() {
+  return (
+    <section className="relative h-screen bg-banking-pattern bg-cover overflow-hidden">
+      <div className="absolute bottom-[35%] left-[15%]">
+        <div className="">
+          <div className="text-white text-6xl tracking-wide leading-relaxed">
+            Banking
+          </div>
+          <p className="font-sans text-2xl">
+            Receive your paycheck, tax returns, and <br />
+            other direct deposits up to two days early <br />
+            using your Cash App routing <br />
+            and account number.
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
