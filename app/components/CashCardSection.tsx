159850caff7367496d0cae9856e37b09926603b5
kushal-khare-official <kushalkhare.official@gmail.com>
2023-09-13 18:45:07 +0530

    Add Cash and Card Boost section

---
+++ app/components/CashCardSection.tsx
@@ -0,0 +1,19 @@
+export default function CashCardSection() {
+  return (
+    <div className="relative h-screen bg-boost-pattern bg-cover overflow-hidden">
+      <div className="absolute bottom-[15%] left-[15%]">
+        <div className="text-cash-app-green text-6xl tracking-wide leading-relaxed">
+          Cash Card <br />
+          &amp; Boost
+        </div>
+        <p className="font-sans text-2xl text-white">
+          The Cash Card is a free, customizable debit <br />
+          card that lets you pay online and in stores. <br />
+          It’s the only way to get Boosts—instant <br />
+          discounts that work at places where you <br />
+          want to spend.
+        </p>
+      </div>
+    </div>
+  )
+}
